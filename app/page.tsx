8c38a6ef14f2f819c6fd1e6c0f6611a21b7e2d63
jack-whatley <wickselm2005@gmail.com>
2023-01-11 21:37:33 +0000

    making front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
 import Link from "next/link";
+import Script from "next/script";
 import styles from "./components/Home.module.css";
 
 export default function Page() {
     return (
         <>
             <div className={styles.main}>
-                <div className={styles.left}>
+                <div className={styles.left} id="left-div">
                     <div className={styles.circle}></div>
                 </div>
                 
                 <div className={styles.center}></div>
                 
-                <div className={styles.right}></div>
+                <div className={styles.right} id="right-div">
+                    <div className={styles.circle}></div>
+                </div>
             </div>
             
-            
+            <Script src="https://code.jquery.com/jquery-3.6.3.slim.min.js" strategy="afterInteractive"/>
+            <Script src="/random.js"/>
         </>
     );
 }
